ec6867ff33fec9253ad70626dc5add8f738e102f
Eryens <david.saigne@hotmail.fr>
2019-09-17 19:05:26 +0200

    added user page

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -72,7 +72,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                with font-awesome or any other icon font library -->
           <li class="nav-item">
             <router-link to="/dashboard" tag="a" class="nav-link" active-class="active" exact>
-              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <i class="nav-icon fas fa-tachometer-alt color-blue"></i>
               <p>
                 Dashboard
               </p>
@@ -80,7 +80,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
           </li>
           <li class="nav-item has-treeview">
             <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-cog"></i>
+              <i class="nav-icon fas fa-cog color-green"></i>
               <p>
                 Management
                 <i class="right fas fa-angle-left"></i>
@@ -88,34 +88,32 @@ scratch. This page gets rid of all links and provides the needed markup only.
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="#" class="nav-link active">
-                  <i class="fas fa-circle nav-icon"></i>
-                  <p>Active Page</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="#" class="nav-link">
-                  <i class="fas fa-circle nav-icon"></i>
-                  <p>Inactive Page</p>
-                </a>
+                <router-link to="/users" tag="a" class="nav-link" active-class="active" exact>
+                  <i class="fas fa-users nav-icon"></i>
+                  <p>Users</p>
+                </router-link>
               </li>
             </ul>
           </li>
           <li class="nav-item">
             <router-link to="/profile" tag="a" class="nav-link" active-class="active" exact>
-              <i class="nav-icon fas fa-user"></i>
+              <i class="nav-icon fas fa-user color-orange"></i>
               <p>
                 Profile
               </p>
             </router-link>
           </li>
           <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-power-off"></i>
-              <p>
-                Logout
-              </p>
+            <a class="nav-link" href="{{ route('logout') }}"
+              onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();">
+              <i class="nav-icon fas fa-power-off color-red"></i>
+              <p>{{ __('Logout') }}</p>
             </a>
+
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
           </li>
         </ul>
       </nav>
